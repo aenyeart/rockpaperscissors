9cec34421cf89faeb3b504c2e77fed99df898434
aenyeart <reallyhardtoforget@gmail.com>
2021-06-30 14:07:21 -0700

    Adds functionality to cancel feature on prompt

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 const playOptions = ['ROCK', 'PAPER', 'SCISSORS'];
 let playerScore = 0;
 let computerScore = 0;
+let cancellation = false; // Used later to end game if user clicks "Cancel"
 
 function computerPlay() {
   return playOptions[Math.floor(Math.random() * 3)];
 }
 
 function playerPlay() {
-  let playerEntry = prompt("Rock, Paper, or Scissors?").toUpperCase();
-  return ( // TODO: This logic can be rewritten to avoid confusing anonymous IIFE below
-    // TODO: Also, write something to account for 'Cancel' on prompt
-    playOptions.includes(playerEntry) ? //if playerEntry value is one of the playOptions
-    playerEntry : //return the entry or
-    (() => {     // if not, immediately invoke this function to alert user of error and prompt again
-      alert(`Sorry, that wasn't one of the choices.`);
-      return playerPlay();
-    })()
-  )
+  let playerEntry = prompt("Rock, Paper, or Scissors?");
+  if (playerEntry === null) {
+    cancellation = true;
+    return;
+  }
+  if (playerEntry !== null && playerEntry !== undefined) {
+    playerEntry = playerEntry.toUpperCase();      // converts entry to allcaps
+    if (!playOptions.includes(playerEntry)) {     // if playerEntry not in playOptions[]
+      alert(`Sorry, that wasn't one of the choices.`);  // alert user of error and prompt again
+      playerEntry = playerPlay();
+    }
+  }
+  return playerEntry; // return the entry
 }
 
 function playRound() {
   let playerSelection = playerPlay();
+  if (cancellation) return;
+
   let computerSelection = computerPlay();
   let win;
-  let finalMsg = '';
+  let roundEndMsg = '';
+
 
   if (playerSelection == computerSelection) return "It's a draw!";
 
@@ -41,17 +48,18 @@ function playRound() {
 
   if (win) {
     playerScore++;
-    finalMsg = `You win this round! ${playerSelection} beats ${computerSelection}!`;
+    roundEndMsg = `You win this round! ${playerSelection} beats ${computerSelection}!`;
   } else {
     computerScore++;
-    finalMsg = `You lose this round! ${computerSelection} beats ${playerSelection}!`;
+    roundEndMsg = `You lose this round! ${computerSelection} beats ${playerSelection}!`;
   }
-  finalMsg += `\nWins: ${playerScore} | Losses: ${computerScore}`
-  return finalMsg;
+  roundEndMsg += `\nWins: ${playerScore} | Losses: ${computerScore}`
+  return roundEndMsg;
 }
 
 function game() {
   while (playerScore < 3 && computerScore < 3) { // I removed playCount because if this is "best of five rounds", then the first player to reach three points wins the game and total non-draw rounds will never exceed 5 anyway.
+    if (cancellation) return; // Aborts gameplay
     console.log(playRound());
   }
   let winLose = (playerScore > computerScore)? "WON" : "LOSE";
